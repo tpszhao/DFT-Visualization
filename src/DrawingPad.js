08f02df79de305d7151df8aef5e46734b54624b5
tpszhao <tonyzhao111@gmail.com>
2019-12-31 23:03:37 -0800

    finished sketch pad

--- src/DrawingPad.js
+++ src/DrawingPad.js
@@ -3,36 +3,49 @@ import React, {useRef, useEffect} from 'react';
 
 function DrawingPad() {
   const canvasRef = useRef(null);
-  const axisRef = useRef(null);
-  const origin = {x:400,y:250};
+  const origin = {x:400,y:250}
+
+  const isDrawing = useRef(false)
+  const prevpos = useRef(null);
 
   useEffect(()=>{
-    const canvas = canvasRef.current;        
-    const axis = axisRef.current;
-    [canvas,axis].forEach(
-      item =>{
-        item.width = 2*origin.x;
-        item.height = 2*origin.y;
-        item.style.position = "absolute";
-        const context = item.getContext('2d');
-        context.beginPath();
-        context.arc(origin.x, origin.y, 50, 0, 2 * Math.PI);
-        context.stroke(); 
-        if (item === axis){
-          context.beginPath();
-          context.arc(origin.x+20, origin.y+40, 50, 0, 2 * Math.PI);
-          context.stroke(); 
-        }
-      }
-    )
-
+    const canvas = canvasRef.current;
+    canvas.width = 2*origin.x;
+    canvas.height = 2*origin.y;
+    const context = canvas.getContext('2d');
+    context.translate(origin.x,origin.y);
   })
 
+  const currentpoint = e =>{
+    const {top,left} = canvasRef.current.getBoundingClientRect();
+    return {x:e.clientX - left - origin.x, y:e.clientY - top - origin.y}
+  }
+
+
+  const mousedown = e => {
+    isDrawing.current = true;
+    prevpos.current = currentpoint(e);
+  }
+
+  const mousemove = e => {
+    if (isDrawing.current){
+      const context = canvasRef.current.getContext('2d');
+      const {x,y} = currentpoint(e);
+      context.beginPath();
+      context.moveTo(prevpos.current.x,prevpos.current.y);
+      context.lineTo(x,y);
+      prevpos.current = {x,y};
+      context.stroke();
+    }
+  }
+
+  const mouseup = e => {
+    isDrawing.current = false;
+  }
+
+
   return (
-    <div>
-      <canvas ref={canvasRef}/>
-      <canvas ref={axisRef}/>
-    </div>
+      <canvas ref={canvasRef} onMouseMove={mousemove} onMouseUp = {mouseup} onMouseDown = {mousedown} onMouseLeave = {mouseup}/>
   )
 }
 
